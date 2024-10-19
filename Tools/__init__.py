d744919a49178a945f2ee30d5e40764f08cb9bfb
Ilya Mevshiy <ilyamevshiy@gmail.com>
2024-10-19 17:45:15 +0300

    Add: doc string

--- Tools/__init__.py
+++ Tools/__init__.py
@@ -1,2 +1,10 @@
+# Tools/__init__.py
+"""
+Общий пакет для готовых функций.
+Модули в этом пакете:
+
+- `download`: Позволяет скачать и разархивировать датасет.
+- `parser`: Парсер сайта госкаталог.
+"""
 from .download import download_zip, extract_zip, get_ya_disk_url
 from .parser import goskatalog_parser, zip_files
