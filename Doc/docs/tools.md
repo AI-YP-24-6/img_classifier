e42e74a6676e102026a8031d1bcac1306e5bf9fd
Ilya Mevshiy <ilyamevshiy@gmail.com>
2024-10-21 22:50:00 +0300

    Add: notebook tool

--- Doc/docs/tools.md
+++ Doc/docs/tools.md
@@ -1,14 +1,19 @@
 ::: Tools
-options:
-heading_level: 2
-docstring_section_style: table
+    options:
+        heading_level: 2
+        docstring_section_style: table
 
 ::: Tools.parser
-options:
-heading_level: 3
-docstring_section_style: table
+    options:
+        heading_level: 3
+        docstring_section_style: table
 
 ::: Tools.download
-options:
-heading_level: 3
-docstring_section_style: table
+    options:
+        heading_level: 3
+        docstring_section_style: table
+
+::: Tools.notebook
+    options:
+        heading_level: 3
+        docstring_section_style: table
---
+++ Tools/notebook.py
@@ -0,0 +1,31 @@
+import json
+from pathlib import Path
+
+
+def set_cell_id(notebook_path: str | Path) -> int:
+    """
+    Обновляет идентификаторы ячейек в файле ноутбука от 1 до N
+    :param notebook_path: путь к файлу ipynb
+    :return: 1 - файл обновлен
+    """
+    if isinstance(notebook_path, str):
+        notebook_path = Path(notebook_path)
+
+    with open(notebook_path, "rt") as f_in:
+        doc = json.load(f_in)
+    cnt = 1
+
+    for cell in doc["cells"]:
+        if "execution_count" in cell:
+            cell["execution_count"] = cnt
+
+            for o in cell.get("outputs", []):
+                if "execution_count" in o:
+                    o["execution_count"] = cnt
+
+        cnt += 1
+
+    with open(notebook_path, "wt") as f_out:
+        json.dump(doc, f_out, indent=1)
+
+    return 1
