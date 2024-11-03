bd5d20bd52ce92e5c1f874ba407ee715f0581321
Ilya <61024440+milia20@users.noreply.github.com>
2024-11-03 22:00:08 +0300

    Exploratory data analysis (#12)
    goskatalog parser, mkdocs, pre-commit
    -----
    Signed-off-by: Ilya Mevshiy <ilyamevshiy@gmail.com>
    Co-authored-by: Ilya-Klim <Klimasch2002@gmail.com>

--- Tools/download.py
+++ Tools/download.py
@@ -7,24 +7,35 @@ import requests
 
 
 def get_ya_disk_url(public_key: str) -> str:
-    """Возвращает ссылку на скачивание файла с https://disk.yandex.ru по публичной общей ссылке"""
-    base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'
+    """Возвращает ссылку на скачивание файла с https://disk.yandex.ru по публичной общей ссылке
+    :param public_key: публичная ссылка на файл
+    :return: ссылка на скачивание
+    """
+    base_url = "https://cloud-api.yandex.net/v1/disk/public/resources/download?"
 
     final_url = base_url + urlencode(dict(public_key=public_key))
     response = requests.get(final_url)
-    download_url = response.json()['href']
+    download_url = response.json()["href"]
     return download_url
 
 
 def download_zip(url: str, path: str | Path) -> Path:
-    """Скачивает zip-файл по ссылке и возвращает путь до него"""
+    """Скачивает zip-файл по ссылке и возвращает путь до него
+    :param url: ссылка на zip-файл с Yandex.Disk
+    :param path: место, куда нужно сохранить zip-файл
+    :return: путь до загруженного разархивированного zip-файла
+    """
     response = requests.get(url)
     path = Path(path)
     return extract_zip(response.content, path)
 
 
 def extract_zip(zip_file: bytes, destination_directory: Path) -> Path:
-    """Извлекает содержимое zip-файла в папку destination_directory"""
+    """Извлекает содержимое zip-файла в папку destination_directory
+    :param zip_file: файл с zip-архивом
+    :param destination_directory: место разархивирования
+    :return: место разархивирования
+    """
     destination_directory.mkdir(parents=True, exist_ok=True)
 
     file = zipfile.ZipFile(io.BytesIO(zip_file))
