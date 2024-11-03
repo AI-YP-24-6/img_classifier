da4d49b994e809f6c3c80b7d2ea43d80b8de5f92
Ilya Mevshiy <ilyamevshiy@gmail.com>
2024-11-03 21:54:32 +0300

    fix: notebook.py

--- Tools/notebook.py
+++ Tools/notebook.py
@@ -11,7 +11,7 @@ def set_cell_id(notebook_path: str | Path) -> int:
     if isinstance(notebook_path, str):
         notebook_path = Path(notebook_path)
 
-    with open(notebook_path, "rt") as f_in:
+    with open(notebook_path, "rt", encoding="utf-8") as f_in:
         doc = json.load(f_in)
     cnt = 1
 
@@ -23,9 +23,9 @@ def set_cell_id(notebook_path: str | Path) -> int:
                 if "execution_count" in o:
                     o["execution_count"] = cnt
 
-        cnt += 1
+            cnt += 1
 
-    with open(notebook_path, "wt") as f_out:
-        json.dump(doc, f_out, indent=1)
+    with open(notebook_path, "wt", encoding="utf-8") as f_out:
+        json.dump(doc, f_out, indent=1, ensure_ascii=False)
 
     return 1
