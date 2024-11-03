eef931d5205e6dde683063c5774035cb8f7fc833
Ilya <61024440+milia20@users.noreply.github.com>
2024-11-03 22:00:08 +0300

    Exploratory data analysis (#12)
    goskatalog parser, mkdocs, pre-commit
    -----
    Signed-off-by: Ilya Mevshiy <ilyamevshiy@gmail.com>
    Co-authored-by: Ilya-Klim <Klimasch2002@gmail.com>

---
+++ Docs/tools.md
@@ -0,0 +1,24 @@
+::: Tools
+    options:
+        heading_level: 2
+        docstring_section_style: spacy
+
+::: Tools.parser
+    options:
+        heading_level: 3
+        docstring_section_style: spacy
+
+::: Tools.download
+    options:
+        heading_level: 3
+        docstring_section_style: spacy
+
+::: Tools.notebook
+    options:
+        heading_level: 3
+        docstring_section_style: spacy
+
+::: Tools.analysis
+    options:
+        heading_level: 3
+        docstring_section_style: spacy
--- Tools/__init__.py
+++ Tools/__init__.py
@@ -1,1 +1,14 @@
-from .download import get_ya_disk_url, download_zip, extract_zip
+# Tools/__init__.py
+"""
+Общий пакет для готовых функций.
+Модули в этом пакете:
+
+- `download`: Позволяет скачать и разархивировать датасет.
+- `parser`: Парсер сайта госкаталог.
+- `notebook`: Позволяет обновлять идентификаторы ячеек в ноутбуке.
+- `analysis`: Позволяет находить все jpeg-файлы в директории.
+"""
+from .analysis import display_images, find_needed_jpeg_files
+from .download import download_zip, extract_zip, get_ya_disk_url
+from .notebook import set_cell_id
+from .parser import goskatalog_parser, zip_files
