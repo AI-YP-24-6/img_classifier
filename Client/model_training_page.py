eea52c67a28f3b809c2729203f1f10393143fa47
MrASK2024 <askrasiukov@edu.hse.ru>
2024-12-28 14:05:17 +0300

    added a request fit model1

--- Client/model_training_page.py
+++ Client/model_training_page.py
@@ -5,30 +5,39 @@ from sklearn.svm import SVC
 import numpy as np
 import requests
 import json
+from backend.app.api.models import FitRequest, ModelInfo
 
+def plt_learning_curve(train_sizes, train_scores, test_scores):
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
 
-
-# def plt_learning_curve():
-#     fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 6), sharey=True)
-
-#     common_params = {
-#         "X": X,
-#         "y": y,
-#         "train_sizes": np.linspace(0.1, 1.0, 5),
-#         "cv": ShuffleSplit(n_splits=50, test_size=0.2, random_state=0),
-#     "score_type": "both",
-#     "n_jobs": 4,
-#         "line_kw": {"marker": "o"},
-#         "std_display_style": "fill_between",
-#         "score_name": "Accuracy",
-#     }
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
     
-#     model = SVC()
-#     for ax_idx, estimator in enumerate(model):
-#         LearningCurveDisplay.from_estimator(model, **common_params, ax=ax[ax_idx])
-#         handles, label = ax[ax_idx].get_legend_handles_labels()
-#         ax[ax_idx].legend(handles[:2], ["Training Score", "Test Score"])
-#         ax[ax_idx].set_title(f"Learning Curve for {estimator.__class__.__name__}")
+    plt.figure(figsize=(10, 6))
+    
+    plt.plot(train_sizes, train_scores_mean, marker='o', label='Тренировочная оценка', color='blue')
+    plt.fill_between(train_sizes, 
+                 train_scores_mean - train_scores_std, 
+                 train_scores_mean + train_scores_std, 
+                 color='blue', alpha=0.2)
+    
+    plt.plot(train_sizes, test_scores_mean, marker='o', label='Тестовая оценка', color='green')
+    plt.fill_between(train_sizes, 
+                 test_scores_mean - test_scores_std, 
+                 test_scores_mean + test_scores_std, 
+                 color='green', alpha=0.2)
+    
+    plt.title("Кривые обучения")
+    plt.xlabel("Размер обучающей выборки")
+    plt.ylabel("Оценка")
+    plt.xticks(train_sizes)
+    plt.yticks(np.arange(0, 1.1, 0.1))
+    plt.ylim(0, 1)
+    plt.legend()
+    plt.grid()
+    st.pyplot(plt)
+    plt.close()
     
 def model_training_page(url_server):
     st.header("Создание новой модели SVC и выбор гиперпараметров")
@@ -37,39 +46,44 @@ def model_training_page(url_server):
     param_c = st.slider("Выберите параметр регуляризации:", 0.1, 30.0, 0.1)
     kernel = st.selectbox("Выберите ядро:", ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed'])
     probability = st.toggle("Включить оценку вероятности")
+    learning_curve = st.toggle("Включить learning_curve")
     
-    data_config = {
-    "hyperparameters": {
-        "svc_C": param_c,
-        "svc_kernel": kernel,
-        "probability": probability
+    fit_request_data = FitRequest(
+        config={
+        "svc__C": param_c,
+        "svc__kernel": kernel,
+        "svc__probability": probability
         },
-    "id": name_model
-    }
+        with_learning_curve = learning_curve
+    )
     
-    config_json = json.dumps(data_config)
-    print(url_server)
-    if "uploaded_file" in st.session_state and st.session_state.uploaded_file is not None:
-        uploaded_file = st.session_state.uploaded_file
-        files = {"file": (uploaded_file.name, uploaded_file.getvalue(), uploaded_file.type)}
-
-        response = requests.post(url_server + '/load_dataset', files=files)
+    fit_json = fit_request_data.model_dump()
+    if st.button(f':red[**Начать обучение модели**]'):
+        with st.spinner("Обучение модели..."):
+            response = requests.post(url_server + "/fit", json=fit_json)
         
-        if response.status_code == 201:
-            st.success("Модель успешно создана!")
-        else:
-            st.error(f"Произошла ошибка: {response.text}")
-    
-    st.subheader("Информация о модели")
-    
-    st.markdown(f"""
-        - **Название модели:** {name_model}
-        - **Параметр C =** {param_c}
-        - **Ядро:** {kernel}
-        - **Оценка вероятности:** {probability}.
-            """)
-    
-    st.subheader("Поученные кривые обучения")
-    # plt_learning_curve()
+            if response.status_code == 201:
+                st.success("Модель успешно обучена!")
+                try:
+                    response_data = json.loads(response.text)
+                    model_info = ModelInfo(**response_data)
+                    st.subheader("Информация о модели")
+                    st.markdown(f"""
+                        - **Название модели:** {model_info.id}
+                        - **Параметр C =** {model_info.hyperparameters['svc__C']}
+                        - **Ядро:** {model_info.hyperparameters['svc__kernel']}
+                        - **Оценка вероятности:** {model_info.hyperparameters['svc__probability']}
+                         """)
+                    
+                    st.subheader("Поученные кривые обучения")
+                    learning_curve = model_info.learning_curve
+                    print(f'Sizes = {learning_curve.train_sizes}')
+                    plt_learning_curve(learning_curve.train_sizes, learning_curve.train_scores, learning_curve.test_scores)
+                    
+                except Exception as e:
+                    st.error(f"Ошибка при парсинге ответа: {e}")
+            else:
+                st.error(f"Произошла ошибка: {response.text}")
+
     
     
