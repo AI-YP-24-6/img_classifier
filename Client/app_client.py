671d9ebfd364c3b414b2c10002ee2d9ca9b374d6
MrASK2024 <askrasiukov@edu.hse.ru>
2024-12-29 08:12:33 +0300

    update sickit 0.25.0 and url_server home pk

--- Client/app_client.py
+++ Client/app_client.py
@@ -7,7 +7,7 @@ from model_inference import model_inference
 from streamlit_option_menu import option_menu
 
 if "url_server" not in st.session_state:
-    st.session_state.url_server = "http://127.0.0.1:8000/api/v1/models"
+    st.session_state.url_server = "http://127.0.0.1:8081/api/v1/models"
 
 st.title("Сервис обучения моделей для классификации фруктов и овощей")
 
