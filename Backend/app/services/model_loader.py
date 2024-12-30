502b1bde1c7de73aafdfa0bbab7fc6bede1d6b02
Ilya Mevshiy <ilyamevshiy@gmail.com>
2024-12-30 15:25:11 +0300

    doc: add Docstrings for CustomUnpickler

--- Backend/app/services/model_loader.py
+++ Backend/app/services/model_loader.py
@@ -1,16 +1,27 @@
 import os
 import pickle
+from typing import Any
+
+from Backend.app.services.pipeline import HogTransformer
 
 MODEL_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "..", "Backend", "data", "baseline.pkl")
 
 
 class CustomUnpickler(pickle.Unpickler):
+    """
+    Класс обертка. Позволяет использовать в pickle-файле классы из других модулей
+    """
 
-    def find_class(self, module, name):
+    def find_class(self, module: str, name: str) -> HogTransformer | Any:
+        """
+        Проверка на соответствие классу и вызов метода pickle родителя
+        :param module: модуль класса
+        :param name: имя класса
+        :return: или наш класс, или класс из pickle
+        """
         if name == "HogTransformer":
-            from Backend.app.services.pipeline import HogTransformer
-
             return HogTransformer
+
         return super().find_class(module, name)
 
 
