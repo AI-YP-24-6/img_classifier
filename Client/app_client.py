f96d4e05f2939cb241f01d91af5ebc268ce4f823
Ilya Mevshiy <ilyamevshiy@gmail.com>
2024-12-31 12:43:58 +0300

    feat: loadenv for Client

--- Client/app_client.py
+++ Client/app_client.py
@@ -1,4 +1,7 @@
+import os
+
 import streamlit as st
+from dotenv import load_dotenv
 from loguru import logger
 from streamlit_option_menu import option_menu
 
@@ -7,11 +10,14 @@ from Client.model_inference import model_inference
 from Client.model_training_page import model_training_page
 from Tools.logger_config import configure_client_logging
 
+# Load the .env file
+load_dotenv()
+
 configure_client_logging()
 
 
 if "url_server" not in st.session_state:
-    st.session_state.url_server = "http://127.0.0.1:54545/api/v1/"
+    st.session_state.url_server = os.getenv("BACKEND_URL", "http://127.0.0.1:54545/api/v1/")
 logger.info("Запуск клиента")
 st.title("Сервис обучения моделей для классификации фруктов и овощей")
 
@@ -27,3 +33,5 @@ elif selected == "Обучение модели":
 elif selected == "Инференс":
     logger.info("Выбрана страница для инференса")
     model_inference(st.session_state.url_server)
+
+logger.info("Завершение работы клиента")
